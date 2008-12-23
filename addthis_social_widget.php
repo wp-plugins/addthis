4935af8ba7177f135282f7d5a2f963ff4b443bd7
_mjk_ <_mjk_@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-23 02:25:01 +0000

    Tagged first release. And crossed fingers.
    git-svn-id: https://plugins.svn.wordpress.org/addthis/tags/1.5.0@81996 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ addthis_social_widget.php
@@ -0,0 +1,264 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+/* 
+* +--------------------------------------------------------------------------+
+* | Copyright (c) 2008 Add This, LLC                                         |
+* +--------------------------------------------------------------------------+
+* | This program is free software; you can redistribute it and/or modify     |
+* | it under the terms of the GNU General Public License as published by     |
+* | the Free Software Foundation; either version 2 of the License, or        |
+* | (at your option) any later version.                                      |
+* |                                                                          |
+* | This program is distributed in the hope that it will be useful,          |
+* | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
+* | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
+* | GNU General Public License for more details.                             |
+* |                                                                          |
+* | You should have received a copy of the GNU General Public License        |
+* | along with this program; if not, write to the Free Software              |
+* | Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA |
+* +--------------------------------------------------------------------------+
+*/
+
+/**
+* Plugin Name: AddThis Social Bookmarking Widget
+* Plugin URI: http://www.addthis.com
+* Description: Help your visitor promote your site! The AddThis Social Bookmarking Widget allows any visitor to bookmark your site easily with many popular services. Sign up for an AddThis.com account to see how your visitors are sharing your content--which services they're using for sharing, which content is shared the most, and more. It's all free--even the pretty charts and graphs.
+* Version: 1.5.0
+*
+* Author: The AddThis Team
+* Author URI: http://www.addthis.com
+*/
+class addthis_social_widget
+{
+    /// Set false if you'd like the pop-up menu instead
+    private $addthis_isdropdown;
+
+    /// Advanced customization options here (e.g., 'addthis_options = "email";'). See http://www.addthis.com/customization
+    private $customization;
+    private $addthis_language;
+
+    /// AddThis publisher ID
+    private $addthis_username;
+
+    /// Share button style (possible styles: share, bookmark, addthis, share-small, bookmark-small, plus)
+    private $addthis_style;
+
+    public static $languages = array('zh'=>'Chinese', 'da'=>'Danish', 'nl'=>'Dutch', 'en'=>'English', 'fi'=>'Finnish', 'fr'=>'French', 'de'=>'German', 'he'=>'Hebrew', 'it'=>'Italian', 'ja'=>'Japanese', 'ko'=>'Korean', 'no'=>'Norwegian', 'pl'=>'Polish', 'pt'=>'Portugese', 'ru'=>'Russian', 'es'=>'Spanish', 'sv'=>'Swedish');
+
+    public static $btnStyles = array(
+                                      'share' => array('img'=>'lg-share-%lang%.gif', 'w'=>125, 'h'=>16),
+                                      'bookmark' => array('img'=>'lg-bookmark-en.gif', 'w'=>125, 'h'=>16),
+                                      'addthis' => array('img'=>'lg-addthis-en.gif', 'w'=>125, 'h'=>16),
+                                      'share-small' => array('img'=>'sm-share-%lang%.gif', 'w'=>83, 'h'=>16),
+                                      'bookmark-small' => array('img'=>'sm-bookmark-en.gif', 'w'=>83, 'h'=>16),
+                                      'plus' => array('img'=>'sm-plus.gif', 'w'=>16, 'h'=>16)
+                                      /* Add your own style here, like this:
+                                        , 'custom' => array('img'=>'http://example.com/button.gif', 'w'=>16, 'h'=>16) */
+                                    );
+
+
+    /**
+    * Adds WP filter so we can append the AddThis button to post content.
+    */
+    function addthis_social_widget($username=null, $style=null)
+    {
+        add_filter('the_content', array(&$this, 'social_widget'));
+        add_filter('admin_menu', array(&$this, 'admin_menu'));
+
+        add_option('addthis_username');
+        add_option('addthis_options', 'email, favorites, digg, delicious, myspace, google, facebook, reddit, live, more');
+        add_option('addthis_isdropdown', true);
+        add_option('addthis_style');
+        add_option('addthis_logo');
+        add_option('addthis_logo_background');
+        add_option('addthis_logo_color');
+        add_option('addthis_brand');
+        add_option('addthis_language', 'en');
+
+        $this->addthis_isdropdown = get_option('addthis_isdropdown') === 'true';
+        
+        if (!isset($style)) $style = get_option('addthis_style');
+        if (strlen($style) == 0) $style = 'share';
+        $this->addthis_style = $style;
+
+        if (!isset($username)) $username = get_option('addthis_username');
+        $this->addthis_username = $username;
+
+        $language = get_option('addthis_language');
+        $this->addthis_language = $language;
+
+        $advopts = array('logo', 'brand', 'language', 'logo_background', 'logo_colors', 'options');
+        $this->customization = '';
+        for ($i = 0; $i < count($advopts); $i++)
+        {
+            $opt = $advopts[$i];
+            $val = get_option("addthis_$opt");
+            if (isset($val) && strlen($val)) $this->customization .= "var addthis_$opt = '$val';";
+        }
+    }
+
+    /**
+    * Appends AddThis button to post content.
+    */
+    public function social_widget($content)
+    {
+        // add nothing to RSS feed
+        if (is_feed()) return $content;
+
+        $pub = $this->addthis_username;
+        $link  = urlencode(get_permalink());
+        $title = urlencode(get_the_title($id));
+
+        $content .= "\n<!-- AddThis Button BEGIN -->\n";
+        if ($this->addthis_isdropdown)
+        {
+            if (isset($pub) || strlen($this->customization)) 
+            {
+                $content .= '<script type="text/javascript">' . (isset($pub) ? "\nvar addthis_pub = '$pub';\n" : "\n") . ($this->customization) . "\n</script>\n";
+            }
+            $content .= <<<EOF
+<div><a href="http://www.addthis.com/bookmark.php" onmouseover="return addthis_open(this, '', '$link', '$title')" onmouseout="addthis_close()" onclick="return addthis_sendto()">{$this->get_button_img()}</a><script type="text/javascript" src="http://s7.addthis.com/js/152/addthis_widget.js"></script></div>
+EOF;
+        }
+        else
+        {
+            $content .= <<<EOF
+<div><a href="http://www.addthis.com/bookmark.php" onclick="window.open('http://www.addthis.com/bookmark.php?pub=$pub&amp;url=$link&amp;title=$title', 'addthis', 'scrollbars=yes,menubar=no,width=620,height=520,resizable=yes,toolbar=no,location=no,status=no'); return false;" title="Bookmark using any bookmark manager!" target="_blank">{$this->get_button_img()}</a></div>
+EOF;
+        }
+        $content .= "\n<!-- AddThis Button END -->";
+        return $content;
+    }
+
+    /**
+    * Generates img tag for share/bookmark button.
+    */
+    private function get_button_img()
+    {
+        $btnStyle = $this->addthis_style;
+        if ($this->addthis_language != 'en')
+        {
+            // We use a translation of the word 'share' for all verbal buttons
+            switch ($btnStyle)
+            {   
+                case 'bookmark':
+                case 'addthis':
+                case 'bookmark-sm':
+                    $btnStyle = 'share';
+            }
+        }
+
+        if (!isset(self::$btnStyles[$btnStyle])) $btnStyle = 'share';
+        $btnRecord = self::$btnStyles[$btnStyle];
+        $btnUrl = (strpos(trim($btnRecord['img']), 'http://') !== 0 ? "http://s7.addthis.com/static/btn/" : "") . $btnRecord['img'];
+             
+        if (strpos($btnUrl, '%lang%') !== false)
+        {
+            $btnUrl = str_replace('%lang%',$this->addthis_language, $btnUrl);
+        }
+        $btnWidth = $btnRecord['w'];
+        $btnHeight = $btnRecord['h'];
+        return <<<EOF
+<img src="{$btnUrl}" width="{$btnWidth}" height="{$btnHeight}" border="0" alt="" />
+EOF;
+    }
+
+    public function admin_menu()
+    {
+        add_options_page('AddThis Plugin Options', 'AddThis', 8, __FILE__, 'addthis_plugin_options');
+    }
+}
+
+function addthis_plugin_options() {
+?>
+    <div class="wrap">
+    <h2>AddThis</h2>
+
+    <form method="post" action="options.php">
+    <?php wp_nonce_field('update-options'); ?>
+
+    <h3>Required</h3>
+    <table class="form-table">
+        <tr valign="top">
+            <th scope="row"><?php _e("AddThis username:", 'addthis_trans_domain' ); ?></th>
+            <td><input type="text" name="addthis_username" value="<?php echo get_option('addthis_username'); ?>" /></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row"><?php _e("Button style:", 'addthis_trans_domain' ); ?></th>
+            <td>
+                <select name="addthis_style">
+                <?php
+                    $curstyle = get_option('addthis_style');
+                    foreach (addthis_social_widget::$btnStyles as $style => $info)
+                    {
+                        echo "<option value=\"$style\"". ($style == $curstyle ? " selected":""). ">$style</option>";
+                    }
+                ?>
+                </select>
+            </td>
+        </tr>
+        <tr valign="top">
+            <th scope="row"><?php _e("Use dropdown menu:", 'addthis_trans_domain' ); ?></th>
+            <td><input type="checkbox" name="addthis_isdropdown" value="true" <?php echo (get_option('addthis_isdropdown') == 'true' ? 'checked' : ''); ?>/></td>
+        </tr>
+    </table>
+    
+    <br />
+    <br />
+    <br />
+
+    <h3>Advanced</h3>
+    See <a href="http://addthis.com/customization">our customization docs</a>.
+    <table class="form-table">
+        <tr valign="top">
+            <th scope="row"><?php _e("Brand:", 'addthis_trans_domain' ); ?></th>
+            <td><input type="text" name="addthis_brand" value="<?php echo get_option('addthis_brand'); ?>" /></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row"><?php _e("Drop-down options (comma-separated):", 'addthis_trans_domain' ); ?></th>
+            <td><input type="text" name="addthis_options" value="<?php echo get_option('addthis_options'); ?>" size="100"/></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row"><?php _e("Language:", 'addthis_trans_domain' ); ?></th>
+            <td>
+                <select name="addthis_language">
+                <?php
+                    $curlng = get_option('addthis_language');
+                    foreach (addthis_social_widget::$languages as $lng=>$name)
+                    {
+                        echo "<option value=\"$lng\"". ($lng == $curlng ? " selected":""). ">$name</option>";
+                    }
+                ?>
+                </select>
+            </td>
+        </tr>
+        <tr valign="top">
+            <th scope="row"><?php _e("Logo:", 'addthis_trans_domain' ); ?></th>
+            <td><input type="text" name="addthis_logo" value="<?php echo get_option('addthis_logo'); ?>" /></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row"><?php _e("Logo background:", 'addthis_trans_domain' ); ?></th>
+            <td><input type="text" name="addthis_logo_background" value="<?php echo get_option('addthis_logo_background'); ?>" /></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row"><?php _e("Logo color:", 'addthis_trans_domain' ); ?></th>
+            <td><input type="text" name="addthis_logo_color" value="<?php echo get_option('addthis_logo_color'); ?>" /></td>
+        </tr>
+    </table>
+
+    <input type="hidden" name="action" value="update" />
+    <input type="hidden" name="page_options" value="addthis_username,addthis_style,addthis_isdropdown,addthis_language,addthis_brand,addthis_options,addthis_logo,addthis_logo_background,addthis_logo_color"/>
+
+    <p class="submit">
+    <input type="submit" name="Submit" value="<?php _e('Save Changes') ?>" />
+    </p>
+
+    </form>
+    </div>
+<?php
+}
+
+
+$addthis &= new addthis_social_widget();
+?>
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== AddThis ===
+Contributors: _mjk_
+Donate link: http://www.addthis.com/
+Tags: share, addthis, social, bookmark, sharing, bookmarking, widget
+Requires at least: 2.3
+Tested up to: 2.7
+Stable tag: 1.5.0
+
+The AddThis Social Bookmarking Widget allows any visitor to bookmark and share your site easily with many popular services. 
+
+== Description ==
+Help your visitor promote your site! The AddThis Social Bookmarking Widget allows any visitor to bookmark your site easily with many popular services. Sign up for an AddThis.com account to see how your visitors are sharing your content--which services they're using for sharing, which content is shared the most, and more. It's all free--even the pretty charts and graphs.
+
+This is the official WordPress plugin by AddThis.com.
+
+== Installation ==
+
+1. Upload `addthis_social_widgetld.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. (Optional) Customize the plugin in the Settings > AddThis menu
+
+== Frequently Asked Questions ==
+
+= Is AddThis free? =
+
+Yep! The features you see today on AddThis will always be free. 
+
+= Do I need to create an account? =
+
+No. You only need to create an account if you want to see how your users are sharing your blog; the sharing itself works the same either way.
+
+== Screenshots ==
+
+1. The button on a sample post.
+2. The settings interface on WordPress 2.7.
