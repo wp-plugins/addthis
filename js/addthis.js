29a1c8e02e7273950fa8efb6d0c12da32b99b5ea
_mjk_ <_mjk_@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-01 18:21:07 +0000

    Ported in final fixes. Almost ready to tag this mama jama.
    git-svn-id: https://plugins.svn.wordpress.org/addthis/trunk@200616 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/addthis.js
+++ js/addthis.js
@@ -1,4 +1,6 @@
-var at_total_shares = 0;
+var at_total_shares = 0,
+    at_populated_svcs = 0,
+    at_populated_content = 0;
 
 function addthis_toggle_tabs(servicesOn) {
     var on = 'services',
@@ -19,6 +21,7 @@ function addthis_toggle_tabs(servicesOn) {
 }
 
 function addthis_populate_services_table(username, password, max, table_id, header_id) {
+    if (!at_populated_svcs) {
         // load service json
         jQuery.getJSON("http://api.addthis.com/analytics/1.0/pub/shares/service.json?suppress_response_codes=true&username="+encodeURIComponent(username)+"&password="+encodeURIComponent(password)+"&callback=?", 
             function(data){
@@ -47,26 +50,30 @@ function addthis_populate_services_table(username, password, max, table_id, head
                     jQuery('#at_data_total_services').text(at_total_shares);
                 } else {
                     jQuery("<tr>").attr("class", "last")
-                    .append(jQuery("<td>").attr("class", "b").text("Total Shares:"))
+                    .append(jQuery("<td>").attr("class", "b atb").text("Total Shares:"))
                     .append(jQuery("<td>"))
                     .append(jQuery("<td>"))
-                    .append(jQuery("<td>").attr("id","at_data_total_services").attr("class", "t").text(at_total_shares))
+                    .append(jQuery("<td>").attr("id","at_data_total_services").attr("class", "t atb").text(at_total_shares))
                     .appendTo(table_id);
                 }
 
                 jQuery("#at_post_total").text(at_total_shares);
                 jQuery('#addthis_tab_table').show();
-                jQuery(table_id+"_table").show();
-            });
+        });
+        at_populated_svcs = 1;
+    }
 }
 
 function addthis_populate_posts_table(username, password, max, table_id, header_id) {
+    if (!at_populated_content) {
         // get the top content data
         jQuery.getJSON("http://api.addthis.com/analytics/1.0/pub/shares/content.json?suppress_response_codes=true&username="+encodeURIComponent(username)+"&password="+encodeURIComponent(password)+"&callback=?", 
             function(data){
                 var other = 0;
                 jQuery.each(data, function(i,item){
-                        var title = decodeURIComponent(item.title); if (title.length > 53) title = title.substr(0, 50) + '...';
+                        var title = item.title;
+                        try { title = decodeURIComponent(title); } catch (e) { title = unescape(title); }
+                        if (title.length > 53) title = title.substr(0, 50) + '...';
                         if ( i < max) {
                             jQuery("<tr>").attr("id", "at_post_data_"+i).attr("class", i == 0 ? "first" : "")
                             .append(jQuery("<td>").attr("class", "b").text(title))
@@ -79,12 +86,14 @@ function addthis_populate_posts_table(username, password, max, table_id, header_
                     jQuery('#at_post_total').text(at_total_shares);
                 } else {
                     jQuery("<tr>").attr("class", "last").attr("id","at_data_total")
-                    .append(jQuery("<td>").attr("class", "b").text("Total Shares:"))
-                    .append(jQuery("<td id='at_post_total'>").attr("class", "t").text(at_total_shares))
+                    .append(jQuery("<td>").attr("class", "b atb").text("Total Shares:"))
+                    .append(jQuery("<td id='at_post_total'>").attr("class", "t atb").text(at_total_shares))
                     .appendTo(table_id);
                 }
 
                 jQuery('#addthis_tab_table').show();
                 jQuery(table_id+"_table").show();
         });
+        at_populated_content = 1;
+    }
 }
