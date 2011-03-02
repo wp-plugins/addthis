e04bc051f8820c0ab2b7dd95d56a3740e071cf39
jorbin <jorbin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-02 02:37:27 +0000

    many updates
    git-svn-id: https://plugins.svn.wordpress.org/addthis/tags/2.0.0@353991 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/addthis.js
+++ js/addthis.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
     var data = {action: "at_show_dashboard_widget"};
 
     $.post(ajaxurl, data, function(response){
-        $( "#dashboard_addthis > .inside > .widget-loading").replaceWith(response);
+        $( "#dashboard_addthis > .inside ").replaceWith('<div class="inside">' + response + '</div>' );
         $( "#at_tabs").tabs();
     });
 
