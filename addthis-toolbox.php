959a4691572d7446ac61e9619b0f42904d94c842
kirk-at-addthis <kirk-at-addthis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-12 17:11:05 +0000

    Tagging 4.0.4 release of the AddThis WordPress plugin
    git-svn-id: https://plugins.svn.wordpress.org/addthis/tags/4.0.4@1111256 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- addthis-toolbox.php
+++ addthis-toolbox.php
@@ -73,8 +73,42 @@ class Addthis_ToolBox
         if ( has_excerpt()) {
             add_filter('the_excerpt', array($this, 'addWidget'));
         } else {
-            add_filter('get_the_excerpt', array($this, 'addWidget'));
+            add_filter('get_the_excerpt', array($this, 'addExcerptCode'));
         }
+    }
+    
+    /**
+     * Prepends a 3-letter non-printing code to a raw excerpt string.
+     *   The order of the code identifies a type of post (archive, category, etc).
+     *
+     *   A piece of javascript later queries for this code, and inserts
+     *   sharetoolbox and recommendedbox divs on either side of the excerpt.
+     *
+     * @param string $content Excerpt contents
+     *
+     * @return string
+     */
+    public function addExcerptCode($content)
+    {   
+        if(preg_match('/[\005-\007]{3}/', $content) == false) {
+            //Homepage = 567
+            if (is_home() || is_front_page()) {
+                return chr(5) . chr(6) . chr(7) . $content;
+            //Page = 576
+            } else if (is_page()) {
+                return chr(5) . chr(7) . chr(6) . $content;
+            //Single Post = 675
+            } else if (is_single()) {
+                return chr(6) . chr(7) . chr(5) . $content;
+            //Category = 657
+            }  else if (is_category()) {
+                return chr(6) . chr(5) . chr(7) . $content;
+            //Archive = 756
+            }  else if (is_archive()) {
+                return chr(7) . chr(5) . chr(6) . $content;
+            }
+        }
+        return $content;
     }
 
     /**
@@ -109,10 +143,10 @@ class Addthis_ToolBox
             } else if (is_single()) {
                 if($at_flag == '' || $at_flag == '1'){
                     $content  = self::_buildDiv(self::AT_ABOVE_POST) . 
-                                self::_buildDiv(self::AT_CONTENT_ABOVE_POST) . 
+                                self::_buildDiv(self::AT_CONTENT_ABOVE_POST, false) . 
                                 $content;
                     $content .= self::_buildDiv(self::AT_BELOW_POST);
-                    $content .= self::_buildDiv(self::AT_CONTENT_BELOW_POST);
+                    $content .= self::_buildDiv(self::AT_CONTENT_BELOW_POST, false);
                 }
             }  else if (is_category()) {
                 if($at_flag == '' || $at_flag == '1'){
@@ -143,13 +177,17 @@ class Addthis_ToolBox
      *
      * @return string
      */
-    private static function _buildDiv($class)
+    private static function _buildDiv($class, $inline_data = true)
     {
         $title = get_the_title();
         $url   = get_permalink();
-        return "<div class='".$class." addthis-toolbox at-wordpress-hide'".
-                   " data-title='".$title."' data-url='".$url."'>".
-                "</div>";
+        if($inline_data == true){
+            return "<div class='".$class." addthis-toolbox at-wordpress-hide'".
+                       " data-title='".$title."' data-url='".$url."'>".
+                    "</div>";
+        } else {
+             return "<div class='".$class." addthis-toolbox at-wordpress-hide'></div>";
+        }
     }
 
     /**
