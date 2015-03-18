8867bc1545ce1497be6bc15486811c6803c1778f
kirk-at-addthis <kirk-at-addthis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-18 16:59:31 +0000

    Release version 4.0.6
    git-svn-id: https://plugins.svn.wordpress.org/addthis/tags/4.0.6@1115390 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- addthis-toolbox.php
+++ addthis-toolbox.php
@@ -64,6 +64,10 @@ class Addthis_ToolBox
     const AT_CONTENT_ABOVE_CAT_PAGE = "at-above-post-cat-page-recommended";
     const AT_CONTENT_ABOVE_ARCH_PAGE = "at-above-post-arch-page-recommended";
 
+    //This associative array keeps relates a post ID to a Title and URL
+    //  Javascript code will later use this to identify and mark up posts
+    public static $postTitlesAndUrlsById;
+    
     /**
      * Initializes the widget class.
      * */
@@ -73,8 +77,55 @@ class Addthis_ToolBox
         if ( has_excerpt()) {
             add_filter('the_excerpt', array($this, 'addWidget'));
         } else {
-            add_filter('get_the_excerpt', array($this, 'addWidget'));
+            add_filter('get_the_excerpt', array($this, 'addExcerptCode'));
         }
+        self::$postTitlesAndUrlsById = array();
+    }
+    
+    /**
+     * Prepends a 3-letter non-printing code to a raw excerpt string.
+     *   The order of the code identifies a type of post (archive, category, etc).
+     *
+     *   A piece of javascript later queries for this code, and inserts
+     *   sharetoolbox and recommendedbox divs on either side of the excerpt.
+     *
+     * @param string $content Excerpt contents
+     *
+     * @return string
+     */
+    public function addExcerptCode($content)
+    {
+        self::$postTitlesAndUrlsById[] = array(
+            'id' => get_the_ID(),
+            'title' => get_the_title(),
+            'url' => get_permalink(),
+            'content' => $content
+        );   
+        
+        if(preg_match('/[\+\-\*]{3}/', $content) == false) {
+            //Homepage = 567
+            if (is_home() || is_front_page()) {
+                return '+' . '-' . '*' . $content;
+            //Page = 576
+            } else if (is_page()) {
+                return '+' . '*' . '-' . $content;
+            //Single Post = 675
+            } else if (is_single()) {
+                return '-' . '*' . '+' . $content;
+            //Category = 657
+            }  else if (is_category()) {
+                return '-' . '+' . '*' . $content;
+            //Archive = 756
+            }  else if (is_archive()) {
+                return '*' . '+' . '-' . $content;
+            }
+        }
+        return $content;
+    }
+    
+    public static function getPostTitlesAndUrls()
+    {
+        return self::$postTitlesAndUrlsById;
     }
 
     /**
@@ -109,10 +160,10 @@ class Addthis_ToolBox
             } else if (is_single()) {
                 if($at_flag == '' || $at_flag == '1'){
                     $content  = self::_buildDiv(self::AT_ABOVE_POST) . 
-                                self::_buildDiv(self::AT_CONTENT_ABOVE_POST) . 
+                                self::_buildDiv(self::AT_CONTENT_ABOVE_POST, false) . 
                                 $content;
                     $content .= self::_buildDiv(self::AT_BELOW_POST);
-                    $content .= self::_buildDiv(self::AT_CONTENT_BELOW_POST);
+                    $content .= self::_buildDiv(self::AT_CONTENT_BELOW_POST, false);
                 }
             }  else if (is_category()) {
                 if($at_flag == '' || $at_flag == '1'){
@@ -143,13 +194,17 @@ class Addthis_ToolBox
      *
      * @return string
      */
-    private static function _buildDiv($class)
+    private static function _buildDiv($class, $inline_data = true)
     {
         $title = get_the_title();
         $url   = get_permalink();
-        return "<div class='".$class." addthis-toolbox at-wordpress-hide'".
-                   " data-title='".$title."' data-url='".$url."'>".
-                "</div>";
+        if($inline_data == true){
+            return "<div class='".$class." addthis-toolbox at-wordpress-hide'".
+                       " data-title='".$title."' data-url='".$url."'>".
+                    "</div>";
+        } else {
+             return "<div class='".$class." addthis-toolbox at-wordpress-hide'></div>";
+        }
     }
 
     /**
